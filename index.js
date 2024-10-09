b52265f8cb7ad0680ef299ffd486f8e2b86ba81e
abrar997 <abraralrawi997.com>
2024-10-09 19:48:14 +0300

    check authentication

---
+++ index.js
@@ -0,0 +1,47 @@
+var loginForm = document.getElementById("login-form");
+
+if (window.location.pathname === "/index.html") {
+  if (!localStorage.getItem("data")) {
+    window.location.href = "/login.html";
+  } else {
+    document.getElementById("logout-link").classList.remove("hidden");
+  }
+}
+
+if (window.location.pathname === "/login.html") {
+  if (localStorage.getItem("data")) {
+    document.getElementById("login-title").innerHTML =
+      "<h1 class='text-4xl font-bold text-main'>You are already login </h1>";
+    document.getElementById("login-btn").style.background = "gray";
+    document.getElementById("login-btn").style.cursor = "not-allowed";
+    loginForm.style.display = "none";
+    setTimeout(() => {
+      window.location.href = "index.html";
+    }, 1500);
+  } else {
+    loginForm.addEventListener("submit", function (e) {
+      var email = document.getElementById("email").value;
+      var password = document.getElementById("password").value;
+
+      e.preventDefault();
+      const user = {
+        email,
+        password,
+      };
+      localStorage.setItem("data", JSON.stringify(user));
+      loginForm.reset();
+      window.location.href = "index.html";
+    });
+  }
+}
+
+if (window.location.pathname === "/logout.html") {
+  document.getElementById("logout-btn").addEventListener("click", function () {
+    if (localStorage.getItem("data")) {
+      localStorage.removeItem("data");
+      window.location.href = "login.html";
+    } else {
+      window.location.href = "login.html";
+    }
+  });
+}
